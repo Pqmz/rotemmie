f875a16d47a373756d2efe7a3573ad0685981f44
Pqmz <30422038+Pqmz@users.noreply.github.com>
2017-07-24 20:24:59 -0400

    Add files via upload

---
+++ movement.java
@@ -0,0 +1,30 @@
+package game;
+
+import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class movement extends JFrame{
+	
+	public movement() {
+		super.setTitle("Game");
+		super.setSize(900,600);
+		super.setLocation(100, 100);
+		super.setResizable(false);
+		super.add(new stuff());
+		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		super.setVisible(true);
+	}
+	
+
+	public static void main(String[] args) {
+		new movement(); 
+	}
+}
---
+++ stuff.java
@@ -0,0 +1,31 @@
+package game;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+
+
+public class stuff extends JPanel implements ActionListener{
+	private Image bob;	
+	
+	public stuff() {
+		super.setDoubleBuffered(true);
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		ImageIcon fuckinghell = new ImageIcon(this.getClass().getResource("bob.gif"));
+		bob = fuckinghell.getImage();
+		
+		Graphics2D g2d = (Graphics2D)g;
+		g2d.drawImage(bob, 100, 100, 100, 100, this);
+	}
+	public void actionPerformed(ActionEvent e) {
+		repaint();		
+	}
+}
